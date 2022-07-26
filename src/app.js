bcf7ea4457db06a09d63c88d4999f93e3eac17b0
pramilaniroshan <pramilaniroshan@gmail.com>
2022-07-27 00:29:13 +0530

    bug fixed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,24 @@ const express = require('express')
 const responseTime = require('response-time')
 const redis = require('redis')
 const axios = require('axios')
+
+
 const app = express()
+//use response time. we will be able to see in the headers of the request a new header called X-Response-Time
+app.use(responseTime())
+
+app.get("/character", async (req, res) => {
+
+    try {
+      const response = await axios.get('https://rickandmortyapi.com/api/character')   
+     
+      console.log(response.data)      
+      return res.status(200).json(response.data) 
+    } catch (err) {   
+      return res.status(500).json({ mmessage: err.mmessage }) 
+    
+    }
+  });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Node server started")
