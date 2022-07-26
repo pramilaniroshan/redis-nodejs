37729727c85051857e911d1584ebd1eb54398f99
pramilaniroshan <pramilaniroshan@gmail.com>
2022-07-27 00:38:33 +0530

    redis connected

--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ const redis = require('redis')
 const axios = require('axios')
 
 
+const runApp = async () => {
+
+ // connect to redis
+  const client = redis.createClient()
+  client.on('error', (err) => console.log('Redis Client Error', err));
+  await client.connect();
+  console.log('Redis connected!')
+
 const app = express()
 //use response time. we will be able to see in the headers of the request a new header called X-Response-Time
 app.use(responseTime())
@@ -24,3 +32,7 @@ app.get("/character", async (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log("Node server started")
 })
+
+}
+
+runApp()
